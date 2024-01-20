5ae3a8e382765c43a4bbe8b2df335d78a91646b9
Kingsley Okpo <kngsley2018@gmail.com>
2024-01-20 16:34:07 +0100

    fixes cloudinary upload issues

--- src/deviceinfo.js
+++ src/deviceinfo.js
@@ -6,7 +6,7 @@ export const device =
         device_name:'',
         spec: '6.5-Inch Super Retina (Gold',
         price:"$10",
-        img:'https://res.cloudinary.com/dpcvlheu9/image/upload/f_auto,q_auto/v1/samples/Odomite_Rentals/dfsnxmz89561rpqjzpas'
+        img:'https://res.cloudinary.com/dpcvlheu9/image/upload/v1705762707/samples/Odomite_Rentals/mijckgbiqu0brprsehrr.jpg'
     },
 
     {
@@ -23,11 +23,45 @@ export const device =
         device_nam:'',
         spec:'Dual SIM - 12GB RAM - 512GB - 5G - Burgundy',
         price:"$40",
-        img:'https://res.cloudinary.com/dpcvlheu9/image/upload/f_auto,q_auto/v1/samples/Odomite_Rentals/dfsnxmz89561rpqjzpas'
+        img:'https://res.cloudinary.com/dpcvlheu9/image/upload/f_auto,q_auto/v1/samples/Odomite_Rentals/ho2lmnlpnql1pe1qr9oq'
 
     },
     {
         id:"3",
+        name:'',
+        device_nam:'Round Table',
+        spec:'Pro - Mondrian (Ti56 + 8',
+        price:"$30",
+        img:'https://res.cloudinary.com/dpcvlheu9/image/upload/v1705764095/samples/Odomite_Rentals/qd6rpcd7au0dkvtod50n.jpg'
+    },
+    {
+        id:"4",
+        name:'Chair',
+        device_name:'',
+        spec: '6.5-Inch Super Retina (Gold',
+        price:"$10",
+        img:'https://res.cloudinary.com/dpcvlheu9/image/upload/f_auto,q_auto/v1/samples/Odomite_Rentals/ho2lmnlpnql1pe1qr9oq'
+    },
+
+    {
+        id:"5",
+        name:'Table',
+        device_nam:'',
+        spec:'16.2 inch" - 32GB - 1TB - 10 Cores - M1 Max - Silver',
+        price:"$45",
+        img:'https://res.cloudinary.com/dpcvlheu9/image/upload/f_auto,q_auto/v1/samples/Odomite_Rentals/dfsnxmz89561rpqjzpas'
+    },
+    {
+        id:"6",
+        name:'Chair',
+        device_nam:'',
+        spec:'Dual SIM - 12GB RAM - 512GB - 5G - Burgundy',
+        price:"$40",
+        img:'https://res.cloudinary.com/dpcvlheu9/image/upload/v1705764095/samples/Odomite_Rentals/l6sb4omgxvpkdickof2v.jpg'
+
+    },
+    {
+        id:"7",
         name:'',
         device_nam:'Round Table',
         spec:'Pro - Mondrian (Ti56 + 8',
