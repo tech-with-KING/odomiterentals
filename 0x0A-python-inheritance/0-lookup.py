8cce36133a5db0d867216d0690e35f9c9533c611
Kingsley Okpo <kngsley2018@gmail.com>
2024-02-05 09:12:46 +0100

    creates files, 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """Returns a list of all the attributes and methods in a class"""
+    return (dir(obj))
--- 0x0A-python-inheritance/main.h
+++
@@ -1,3 +0,0 @@
-#ifndef LIBMY_A
-#define LIBMY_A
-#endif
