65a2e6b34b15d1b69d558cf8cc06f5af4d6350cf
Kingsley Okpo <kngsley2018@gmail.com>
2024-02-12 18:24:50 +0100

    updates the winter special deal

--- src/body/promotion.js
+++ src/body/promotion.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import "./promotion.css"
 const sampleHeroBanner = {
   smallText: 'Winter Promotion',
-  midText: '50% Off All Rentals',
+  midText: '50% Off All Second Day Rentals',
   largeText1: 'Enjoy Your Winter Adventures',
   image: 'https://res.cloudinary.com/dpcvlheu9/image/upload/v1706283742/OdomiteRentals/Hero_Images/vddb37jv8bwcjo2k9w6o.jpg',
   product: 'winter-special-product',
   buttonText: 'Call Now To Book',
-  desc: 'Hurry up! Limited-time offer. Get 50% off on all rentals this winter season.',
+  desc: 'Hurry up! Limited-time offer. Get 50% off on all second day rentals this winter season.',
 };
 
 const PromotionalPage = ({ heroBanner = sampleHeroBanner }) => {
