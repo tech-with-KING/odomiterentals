39898083db6d7e00f4c26c5228e6043315a9f3cc
Kingsley Okpo <kngsley2018@gmail.com>
2024-03-03 18:38:21 +0100

    updates prices

--- src/body/body.js
+++ src/body/body.js
@@ -67,7 +67,7 @@ const Brands = (props)=>{
 	{
 		id: 2,
 		color: 'about_heading',
-		link: 'https://wa.me/c/18622306639',
+		link: 'https://wa.me/p/6709219215829483/18622306639',
 		item: <WhatsApp style={{ color: '#00ACEE' }} />,
 	},
 	{
